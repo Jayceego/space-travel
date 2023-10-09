8a7c7c66afdde36aa9f8c4c629fcb51947e17293
Jayceego <itsmejcgo@gmail.com>
2023-10-09 18:29:50 +0800

    404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,9 +1,9 @@
-// import React from "react"
+import React from "react"
 
-// export default function Notfound() {
-//   return (
-//     <div>
-//       <h1>NotFound</h1>
-//     </div>
-//   )
-// }
+export default function Notfound() {
+  return (
+    <div>
+      <h1>NotFound</h1>
+    </div>
+  )
+}
